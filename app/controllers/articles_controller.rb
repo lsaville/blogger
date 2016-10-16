fd3b6ba86ba3deac88215fb791f95ec230692946
Lee Saville <l.w.saville@gmail.com>
2016-10-16 13:36:02 -0600

    Changed the show method to see how not to do it

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,8 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comment.article_id = @article.id
   end
 
   def new
@@ -18,7 +20,7 @@ class ArticlesController < ApplicationController
     @article.save
 
     flash.notice = "Article '#{@article.title}' Created!"
-    
+
     redirect_to article_path(@article)
   end
 
